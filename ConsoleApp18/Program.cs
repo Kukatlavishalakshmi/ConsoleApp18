8e1026717ab7dc23a6c7b32b451886e155fe3aeb
Kukatla Vishalakshmi <Kukatla.Vishalakshmi@ibm.com>
2022-03-03 11:23:57 +0530

    Add project files.

---
+++ ConsoleApp18/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp18
+{
+	internal class queue
+	{
+		public static void Main()
+		{
+			Queue<int> callerIds = new Queue<int>();
+			callerIds.Enqueue(1);
+			callerIds.Enqueue(2);
+			callerIds.Enqueue(3);
+			callerIds.Enqueue(4);
+
+			foreach (var id in callerIds)
+				Console.WriteLine(id);
+			Console.ReadLine();
+		}
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
